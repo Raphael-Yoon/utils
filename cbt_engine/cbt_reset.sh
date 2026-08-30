0e5c485fa7c3f4f347b57004dd7e736e2e94e410
Raphael <newsist@naver.com>
2026-08-30 23:53:13 +0900

    v1.89 - 서버 실행/제어 쉘 스크립트(*.sh) gitignore 등록 및 Git 추적 제외

--- cbt_engine/cbt_reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# "재설정"은 cbt_start.sh가 매번 수행하는 강제 동기화(git reset --hard)와 동일하므로,
-# 별도로 git 동기화를 중복 수행하지 않고 시작 스크립트에 그대로 위임한다.
-# (예전에는 여기서도 fetch/reset을 한 번 더 실행해 로그가 두 번 찍히는 문제가 있었음)
-cd /home/raphael/Dev/pythons/utils/cbt_engine
-./cbt_start.sh
--- health_check/daily_health_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Daily Health Check runner script for cron job
-# Developed by Dev Team 4
-
-# Move to workspace directory
-cd /home/yuju/Dev/Pythons
-
-# Activate virtual environment and run python health check
-/home/yuju/Dev/Pythons/.venv/bin/python /home/yuju/Dev/Pythons/utils/health_check/daily_health_check.py >> /home/yuju/Dev/Pythons/utils/health_check/health_check.log 2>&1
